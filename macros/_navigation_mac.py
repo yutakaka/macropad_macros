987ba617f13e5a77921bd22c0354929857d83f09
yutakaka <yutakk77@hotmail.com>
2024-05-30 10:29:30 -0700

    Update _navigation_mac.py
    Add shortcut keys in need, replacing ones used less

--- macros/_navigation_mac.py
+++ macros/_navigation_mac.py
@@ -4,24 +4,24 @@ from adafruit_hid.keycode import Keycode # REQUIRED if using Keycode.* values
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 app = {                # REQUIRED dict, must be named 'app'
-    'name' : 'nav-Mac', # Application name
+    'name' : 'Mac Desktop', # Application name
     'macros' : [       # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x6600CC, 'VolUp', [ConsumerControlCode.VOLUME_INCREMENT]),
+        (0x000000, 'Empty', []),
         (0x000020, 'HOME', [Keycode.HOME]),
         (0x202000, 'PgUp', [Keycode.PAGE_UP]),
         # 2nd row ----------
-        (0x6600CC, 'VolDn', [ConsumerControlCode.VOLUME_DECREMENT]),
-        (0x000020, 'END', [Keycode.END]),
+        (0x800000, 'DEL', [Keycode.DELETE]),
+        (0x000020, 'Speak', [Keycode.SHIFT, Keycode.CONTROL, Keycode.S]),
         (0x202000, 'PgDn', [Keycode.PAGE_DOWN]),
         # 3rd row ----------
-        (0x006699, 'ESC', [Keycode.ESCAPE]),
-        (0x800000, 'Speak', [Keycode.SHIFT, Keycode.CONTROL, Keycode.S]),
-        (0x004000, 'Enter', [Keycode.ENTER]),
+        (0x006699, 'PrevApp', [Keycode.COMMAND, Keycode.LEFT_SHIFT,Keycode.TAB]),
+        (0xee6600, '^', [Keycode.CONTROL, Keycode.UP_ARROW]),
+        (0x004000, 'NextApp', [Keycode.COMMAND,Keycode.TAB]),
         # 4th row ----------
         (0x202020, '<WSpace', [Keycode.CONTROL, Keycode.LEFT_ARROW]),
-        (0xee6600, '^', [Keycode.CONTROL, Keycode.UP_ARROW]),
+        (0xee6600, 'AppWin', [Keycode.CONTROL, Keycode.DOWN_ARROW]),
         (0x202020, 'WSpace>', [Keycode.CONTROL, Keycode.RIGHT_ARROW]),
         # Encoder button ---
         (0x000000, '', [Keycode.ESCAPE])
